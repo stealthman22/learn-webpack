2dd622ccaa4597b36fd67c4552303ec47996304f
stealthman22 <tdotonline@gmail.com>
2019-12-22 00:15:29 +0000

    updating files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -15,7 +15,17 @@ module.exports = {
         hot: true,
     },
     module: {
+
         rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: true }
+                    }
+                ]
+            },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
@@ -23,7 +33,7 @@ module.exports = {
         ],
     },
     plugins: [
-        new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Development'
         }),
