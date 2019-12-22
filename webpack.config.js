cbf5412d973a00ba8e23fc7660a80370e0dd9e77
stealthman22 <tdotonline@gmail.com>
2019-12-22 23:30:30 +0000

    Editing config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -33,7 +33,9 @@ module.exports = {
         ],
     },
     plugins: [
-        // new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({
+
+        }),
         new HtmlWebpackPlugin({
             title: 'Development'
         }),
